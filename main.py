232e0f19ff5a34b280025d52b2f3d5bad2311725
richard <richard@richard.do>
2013-06-18 00:09:34 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 
 
 def pascal(n):
-	prev_row = []
-	pascal = []
+    prev_row = []
+    pascal = []
 
-	for row_number in range(n):
-		current_row_width = row_number + 1
-		if row_number == 0:
-			row = [1]
-		else:
-			row = []
-			for i in range(current_row_width):
+    for row_number in range(n):
+        current_row_width = row_number + 1
+        if row_number == 0:
+            row = [1]
+        else:
+            row = []
+            for i in range(current_row_width):
                 # 1's
-				if i == current_row_width-1 or  i == 0:
-					row.append(1)
-				else:
-					# calc total
-					row.append(prev_row[i-1] + prev_row[i])
+                if i == current_row_width-1 or i == 0:
+                    row.append(1)
+                else:
+                    # calc total
+                    row.append(prev_row[i-1] + prev_row[i])
 
-		prev_row = row
-		pascal.append(row)
-	return pascal
+        prev_row = row
+        pascal.append(row)
+    return pascal
 
 if __name__ == '__main__':
     p = pascal(10)
